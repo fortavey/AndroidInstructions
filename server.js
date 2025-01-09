6641782c0d1c7975504c2940d1c7be16c5c53f7f
fortavey <fortavey@gmail.com>
2025-01-09 08:57:53 +0300

    server static

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'build', 'static')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res, next) => {
     res.status(200)
