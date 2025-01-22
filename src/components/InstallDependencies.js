95cb0cd995cdd5a8f8304b1758e66a0ec79cd7ee
fortavey <fortavey@gmail.com>
2025-01-22 12:47:26 +0300

    add dependencies

--- src/components/InstallDependencies.js
+++ src/components/InstallDependencies.js
@@ -12,7 +12,7 @@ function InstallDependencies({ appName, upVersion}) {
         <div style={styles.desc}>Работа в терминале. Удостоверится что выбрана папка <span style={styles.codeSpan}>{removeSpaceFromAppName()}</span>
         <br />Вводим команду для установки зависимостей приложения:</div>
         <div style={styles.code}>
-        npm i @fortawesome/fontawesome-svg-core @fortawesome/react-native-fontawesome @react-native-async-storage/async-storage @react-navigation/bottom-tabs @react-navigation/drawer @react-navigation/native @react-navigation/stack react-native-gesture-handler react-native-qrcode-svg react-native-reanimated react-native-safe-area-context react-native-screens react-native-svg react-native-vector-icons
+        npm i @fortawesome/fontawesome-svg-core @fortawesome/react-native-fontawesome @react-native-async-storage/async-storage @react-navigation/bottom-tabs @react-navigation/drawer @react-navigation/native @react-navigation/stack react-native-gesture-handler react-native-qrcode-svg react-native-reanimated react-native-safe-area-context react-native-screens react-native-svg react-native-vector-icons mobx mobx-react-lite @fortawesome/free-solid-svg-icons @react-navigation/native-stack
         </div>
         <div style={styles.desc}>Дожидаемся окончания процесса</div>
         <button style={styles.button} onClick={upVersion}>Готово</button>
